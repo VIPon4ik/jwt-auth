53a0638451fa02822a5a4ca026822ec7e9be74d6
VIPon4ik <umniyd@gmail.com>
2024-01-20 01:37:54 +0200

    Created button

--- src/shared/UI/form/form.component.ts
+++ src/shared/UI/form/form.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { InputComponent } from '../input/input.component';
+import { ButtonComponent } from '../button/button.component';
 
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
