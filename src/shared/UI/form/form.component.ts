b28cd84009b6c38ad16a16f419b49996d6447efd
VIPon4ik <umniyd@gmail.com>
2024-01-20 17:47:25 +0200

    Made custom checkbox

--- src/shared/UI/form/form.component.ts
+++ src/shared/UI/form/form.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { InputComponent } from '../input/input.component';
 import { ButtonComponent } from '../button/button.component';
-
+import { CheckboxComponent } from '../checkbox/checkbox.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, CheckboxComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
