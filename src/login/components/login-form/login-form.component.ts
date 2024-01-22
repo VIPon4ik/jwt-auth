34c520af921c3ffc4b706009a641740e947f8f27
VIPon4ik <umniyd@gmail.com>
2024-01-22 14:19:22 +0200

    Made login form

--- src/login/components/login-form/login-form.component.ts
+++ src/login/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../../../shared/components/form/form.component';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -9,5 +10,14 @@ import { FormComponent } from '../../../shared/components/form/form.component';
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  constructor(private formBuilder: FormBuilder) { }
 
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.minLength(8), Validators.maxLength(32), Validators.required]],
+  });
+
+  onSubmit = () => {
+    console.log(this.loginForm);
+  }
 }
